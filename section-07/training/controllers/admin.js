5f2f8cbd95c3e7a7a20f8b51affbc1052a059917
Giovanni Almeida <69661645+GiovanniBot@users.noreply.github.com>
2023-03-21 01:06:05 -0300

    finishing section 8

--- section-07/training/controllers/admin.js
+++ section-07/training/controllers/admin.js
@@ -6,12 +6,16 @@ exports.getAddProduct = (req, res, next) => {
       path: '/admin/add-product',
       formsCSS: true,
       productCSS: true,
-      activeAddProduct: true
+      activeAddProduct: true,
     });
 };
   
 exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title);
+    const title = req.body.title;
+    const imageUrl = req.body.imageUrl;
+    const price = req.body.price;
+    const description = req.body.description;
+    const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
 };
--- section-07/training/controllers/shop.js
+++ section-07/training/controllers/shop.js
@@ -23,7 +23,14 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     pageTitle: 'Your Cart',
-    path: '/',
+    path: '/cart',
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    pageTitle: 'Your Orders',
+    path: '/orders',
   });
 };
 
